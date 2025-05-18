0c35fc1fc2d3b41f2a95f0f0857da7bd21579fad
Arjun <arjunchinnu604@gmail.com>
2025-05-18 19:08:37 +0530

    Add files via upload

--- code.js
+++ code.js
@@ -5,11 +5,12 @@ let btn = document.querySelector(".btn");
 
 function generate(){
     qrimg.src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+qrtext.value;
+     qrtext.value = "";
 }
 
 btn.onclick = () => {
     generate();
-    qrtext.value = "";
+    
 
 };
 
